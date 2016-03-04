b6316182f210d44d98d8d53baf699b17d22dce8c
juanortiz10 <juanjo.jjo94@gmail.com>
2016-03-04 15:44:05 -0600

    Intervals Working

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -17,15 +17,22 @@ var Maps = React.createClass({
   //Default props or variables to set into the map
   getInitialState: function(){
     return{
-      markers: [], info: '', map: null
+      markers: [],
+      info: '',
+      counter: 0,
+      map: null,
+      positions: []
     };
   },
   getDefaultProps: function () {
       return {
           initialZoom: 6,
-          cameraInterval,
+          modifiedZoom: 15,
+          latitude: 22.133977,
+          longitude: -101.448995,
           source: 'http://192.168.11.148:8000/point/all',
-          interval : 60
+          interval : 60000,
+          cameraInterval: 10000
       };
   },
   //This will run when DOM be ready
@@ -39,13 +46,15 @@ var Maps = React.createClass({
       map = new google.maps.Map(ReactDOM.findDOMNode(this), mapOptions);
       this.setState({map: map});
       this.getData();
+      setInterval(this.changeCamera, this.props.cameraInterval);
   },
   getData: function(){
-    var markerList = [];
+    var markerList = [], coordinatesList=[];
     this.serverRequest = $.get(this.props.source, function (result) {
         for(var i = 0; i < result.length; i++){
             var lat = result[i].latitude;
             var lng = result[i].longitude;
+            var coordinates = [lat,lng];
             var hospital = result[i].name;
             var tickets = result[i].indicator;
             var info = "<h2>"+ result[i].name + "</h2>" +
@@ -54,8 +63,9 @@ var Maps = React.createClass({
                         "<p><h5>Teléfono: </h5>" + result[i].phone + " </p> " +
                         "<p><h5>Tickets: </h5>" + result[i].indicator + " </p> ";
             markerList.push(this.createMarker(lat,lng, hospital,this.state.map, info, tickets));
+            coordinatesList.push(coordinates);
         }
-        this.setState({markers: markerList});
+        this.setState({markers: markerList, positions: coordinatesList});
       }.bind(this));
   },
   getUpdatedData: function(){
@@ -117,9 +127,23 @@ var Maps = React.createClass({
       });
       return mark;
   },
+  changeCamera: function(){
+    var map = this.state.map;
+    var counter = this.state.counter;
+    map.setCenter(new google.maps.LatLng(this.state.positions[counter][0],this.state.positions[counter][1]));
+    console.log(this.state.positions[counter][0],this.state.positions[counter][1]);
+    map.setZoom(this.props.modifiedZoom);
+    counter++;
+    if(counter >= this.state.positions.length){
+        this.setState({counter: 0});
+    }else{
+        this.setState({counter: counter});
+    }
+  },
   //Render
   render: function () {
-      setInterval(this.getUpdatedData,60000);
+      setInterval(this.getUpdatedData, this.props.interval);
+
         return (
             <div className='map-gic'></div>
         );
